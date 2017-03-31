14436aef4f114be575e99c50571ba136932be272
eelizanjr <eelizanjr@gmail.com>
2017-03-31 02:22:31 -0400

    new node files

---
+++ notes-node/notes.js
@@ -0,0 +1,23 @@
+// notes will store all of the exports mainly the functions which will be called by app.js
+
+console.log('Starting notes.js');
+
+// lists all the moduels
+// console.log(module);
+
+// exports is a property of the module object so we can set age to be 25 and make a new property 
+// will be called as notes.age from the fs.appendFile
+// module.exports.age = 25;
+
+// exporting a function 
+// use => for anonymous functions
+module.exports.addNote = () => {
+    console.log('addnote');
+    return 'New note';
+};
+
+module.exports.add = (a, b) => {
+    return a + b;
+};
+
+// require a package you installed from npm
